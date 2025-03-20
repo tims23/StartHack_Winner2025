338078623d6acbc6c406729924d7a386c9eb9e38
Tim Saßmannshausen <tisa@Tims-MacBook-Air.local>
2025-03-20 17:24:21 +0100

    GDSYNKT-9: added daisyui

--- src/app/page.js
+++ src/app/page.js
@@ -97,6 +97,20 @@ export default async function Home() {
           />
           Examples
         </a>
+        <div className="card bg-base-100 w-96 shadow-sm">
+  <figure>
+    <img
+      src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+      alt="Shoes" />
+  </figure>
+  <div className="card-body">
+    <h2 className="card-title">Card Title</h2>
+    <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
+    <div className="card-actions justify-end">
+      <button className="btn btn-primary">Buy Now</button>
+    </div>
+  </div>
+</div>
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
           href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
